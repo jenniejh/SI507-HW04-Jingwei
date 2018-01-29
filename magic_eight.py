37b3169d7d57348f5fe670c10b82078bd25f9803
Jennie <jingweihu.jh@gmail.com>
2018-01-29 17:40:38 -0500

    check_question

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,16 @@
 
-text = input("What is your question?")
+while True:
+
+    text = input("What is your question?")
+
+
+    if text.endswith("?"):
+	    print("Yes, this is a question.")
+
+    elif text == "quit":
+	    break
+
+    else:
+	    print("I’m sorry, I can only answer questions.")
+
+
